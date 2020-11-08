93418419f446090c22558157be18970897facf2e
danielx285 <daniel.carlos.melo@ccc.ufcg.edu.br>
2020-11-08 05:28:13 -0300

    Sleep adicionado e explicações do contexto

--- funcoesCronologicas.hs
+++ funcoesCronologicas.hs
@@ -1,16 +1,28 @@
-import Text.Printf
-import Data.Time
+import Control.Concurrent --Para usar o sleep
+import Text.Printf 
+import Data.Time --Para usar a contagem de tempo
 
 main = do
+    --Exemplo de execução de um sleep de 1 segundo
+    threadDelay 1000000
+
     --Inicio da contagem de tempo
     inicio <- getCurrentTime
+    
     {-
-    escopo de código que deve ser contado deve ser colocado aqui
+    Escopo de código que deve ser contado deve ser colocado aqui
+    Ex : 
+	a <- getLine
+	b <- getLine
+	print a ++ b ++ a
     -}
-    c <- getLine
+    
+    palavra <- getLine
     --Final da contagem de tempo
     final <- getCurrentTime
-    --diferença de tempo representada por um número racional
+    
+    --Diferença de tempo representada por um número racional
     let difTempo = (realToFrac (toRational(diffUTCTime final inicio)))
+    
     print difTempo
 
