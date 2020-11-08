1a5da22f9163fad3556ad6b2745ab0e69ff5595e
lucasbatista036 <lucas.batista.vieira@ccc.ufcg.edu.br>
2020-11-07 22:15:03 -0300

    Adicionado arquivos de texto e leitura, no entanto não foi adicionado no main

---
+++ arquivos.hs
@@ -0,0 +1,97 @@
+import System.IO
+import Data.List
+import System.Process
+
+splitIt :: String -> [String]
+splitIt palavra = words palavra
+
+converteInteiro :: [String] -> [Int]
+converteInteiro = map read
+
+ordena :: [(Int,[Char])] -> [(Int,[Char])]
+ordena lista = sort lista
+
+excluiMenor :: [(Int,[Char])] -> [(Int,[Char])]
+excluiMenor (x:xs) = xs
+
+somaEspaco :: String -> String
+somaEspaco frase = frase ++ " "
+
+mostraHanking :: IO ()
+mostraHanking = do
+       conteudo <- readFile "rankingNumeros.txt"
+       putStrLn conteudo
+
+
+main :: IO ()
+main = do 
+       -- LeituraDanielPython
+       conteudo <- readFile "linguagemPython.txt"
+       let listaPython = splitIt conteudo
+
+       -- LeituraDanielJava
+       conteudo <- readFile "linguagemJava.txt"
+       let listaJava = splitIt conteudo
+
+       -- LeituraDanielHaskell
+       conteudo <- readFile "linguagemHaskell.txt"
+       let listaHaskell = splitIt conteudo
+
+       -- Hanking
+       conteudo <- readFile "numeros.txt"
+
+       let l = splitIt conteudo
+       let inteiro = converteInteiro l
+       let valorNovo = 7                         --  pontuação nova
+       let novoNome = "DEU"                      --  nome da pessoa com a nova pontuação
+
+       let novaListaInteiros = valorNovo : inteiro
+       
+       conteudo <- readFile "nomes.txt"
+
+       let listaNomes = splitIt conteudo
+       let novaListaNomes = novoNome : listaNomes 
+
+       print listaNomes
+       callCommand "clear"
+       
+       let hankingDesordenado = zip novaListaInteiros novaListaNomes
+
+       let hankingOrdenado = ordena hankingDesordenado
+       let hankingAtualizado = excluiMenor hankingOrdenado
+
+       let tuplaUm = hankingAtualizado !!0
+       let tuplaDois = hankingAtualizado !!1
+       let tuplaTres = hankingAtualizado !!2
+       
+       let nomeTres = snd tuplaTres
+       let nomeDois = snd tuplaDois
+       let nomeUm = snd tuplaUm
+
+       let valorTres = fst tuplaTres
+       let valorDois = fst tuplaDois
+       let valorUm = fst tuplaUm
+       
+       let nomeEspacoTres = somaEspaco nomeTres
+       let nomeEspacoDois = somaEspaco nomeDois
+
+       let valorTresEspaco = somaEspaco (show valorTres)
+       let valorDoisEspaco = somaEspaco (show valorDois)
+       
+       writeFile "nomes.txt" nomeEspacoTres
+       appendFile "nomes.txt" nomeEspacoDois
+       appendFile "nomes.txt" nomeUm
+
+       writeFile "numeros.txt" valorTresEspaco
+       appendFile "numeros.txt" valorDoisEspaco
+       appendFile "numeros.txt" (show valorUm)
+
+       writeFile "rankingNumeros.txt" "Ranking: \n"
+       appendFile "rankingNumeros.txt" ("1. " ++ nomeTres)
+       appendFile "rankingNumeros.txt" (" ------------ " ++ (show valorTres) ++ " Pontos")
+       appendFile "rankingNumeros.txt" ("\n2. " ++ nomeDois)
+       appendFile "rankingNumeros.txt" (" ------------ " ++ (show valorDois) ++ " Pontos")
+       appendFile "rankingNumeros.txt" ("\n3. " ++ nomeUm)
+       appendFile "rankingNumeros.txt" (" ------------ " ++ (show valorUm) ++ " Pontos")
+
+
