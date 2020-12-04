a790f11d263f8cd6d2c3c7f46d5ffd8ec172d389
gabrielgns <gabriel.nascimento.santos@ccc.ufcg.edu.br>
2020-12-04 03:19:06 -0300

    Função de escrever no arquivo modificada

--- paradigma_logico/playground/inserir_pontuacao_ranking.pl
+++ paradigma_logico/playground/inserir_pontuacao_ranking.pl
@@ -1,8 +1,26 @@
-escrever(Nome, Pontuacao):-
+escrever(NomePontuacao):-
     open('ranking.txt', write, Out),
-    concat(Nome, "  ->   ", Parte1),
-    concat(Parte1, Pontuacao, Final),
+    ordena(NomePontuacao, TresPrimeiros),
+    [Primeiro, Segundo, Terceiro|_] = TresPrimeiros,
+    
+    Primeiro = (Num1, Nome1),
+    concat(Nome1, "  ->   ", Parte11),
+    concat(Parte11, Num1, Parte12),
+    concat(Parte12, "\n", Final),
+    
+    Segundo = (Num2, Nome2),
+    concat(Nome2, "  ->   ", Parte21),
+    concat(Parte21, Num2, Parte22),
+    concat(Parte22, "\n", Final2),
+    
+    Terceiro = (Num3, Nome3),
+    concat(Nome3, "  ->   ", Parte31),
+    concat(Parte31, Num3, Parte32),
+    concat(Parte32, "\n", Final3),
+    
     write(Out, Final),
+    write(Out, Final2),
+    write(Out, Final3),
     close(Out).   
 
 ler:-
@@ -14,6 +32,11 @@ ler:-
     nl,
     close(In).
 
-main:- read(Nome),
-    read(Pontuacao),
-    escrever(Nome, Pontuacao).
+ordena(ParNomePontuacao, TresPrimeiros):-
+    sort(ParNomePontuacao, ListaOrdenada),
+    reverse(ListaOrdenada, Inversa),
+    [Primeiro, Segundo, Terceiro|_] = Inversa,
+    TresPrimeiros = [Primeiro, Segundo, Terceiro].
+
+main:-
+    escrever([(20, gabriel), (2000, joaozinho), (206, maria), (100, guilherme)]).
