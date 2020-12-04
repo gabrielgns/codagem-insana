c6f90400f8376c52a91d1e310841c5e5caeb7332
Maurício Marques <mauricio.monte@ccc.ufcg.edu.br>
2020-12-04 00:21:43 -0300

    Retira main desnecessário do modulo temporizador

--- paradigma_logico/modulos/temporizador.pl
+++ paradigma_logico/modulos/temporizador.pl
@@ -5,11 +5,3 @@ tempo(Key, Value) :-
     get_time(Stamp),
     stamp_date_time(Stamp, DateTime, local),
     date_time_value(Key, DateTime, Value).
-
-
-main:-
-    tempo(second, T0),
-    read(I),
-    tempo(second, T1),
-    T is T1 - T0,
-    write(T).
